11c5b501a15cbec037a3ec57b08cd6e962fe3196
Sara Malik <sm4418520@gmail.com>
2020-04-23 17:33:53 -0700

    my first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <form action="">
+        {{csrf_field()}}
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+
+
+                    You are logged in! hhkjbjmb
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </form>
+@endsection
