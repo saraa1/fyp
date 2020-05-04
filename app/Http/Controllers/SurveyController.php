0e0ee92aed10c1ee8bca2aeeef93896480f61e9a
saraa1 <38456694+saraa1@users.noreply.github.com>
2020-05-05 01:06:27 +0500

    Add files via upload

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -5,61 +5,51 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ValidateSurveyRequest;
 use App\Questionaire;
+use App\Survey;
 use App\SurveyResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use  App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
 
 class SurveyController extends Controller
 {
     //
     use ValidatesRequests;
-    public function show(Questionaire $questionaire,$slug){
+
+    public function show(Questionaire $questionaire, $slug)
+    {
 
         $user = Auth::user();
         $questionaire->load('question.answer');
-       return view('survey.show', compact('questionaire','slug','user'));
+        return view('survey.show', compact('questionaire', 'slug', 'user'));
 
     }
 
-    public function store(Questionaire $questionaire,$slug){
+    public function store(Questionaire $questionaire, $slug)
+    {
 
 
-
-      //dd(request()->all());
-//      $responses=request()->get('responses');
-//      //dd($responses);
-//        $data= $questionaire->survey()->create($data['survey']);
-      //dd($questionaire->survey()->save($data['survey']));
-       // dd(\request()->responses);
-      // $survey->response()->create();
-////       // SurveyResponse::createMany($data['responses']);
-////
+        $user = Auth::user();
+        //dd(request()->get('answer'));
+        $loop = request()->get('answer');
+        $id = $questionaire->id;
+        $sur = Survey::create(['questionaire_id' => $id, 'name' => $user->name, 'email' => $user->email]);
 //
-//        return redirect('/survey_response');
-//        $data['answer_id']=(request()->has('responses'));
-//        dd($data);
-//        $survey=$questionaire->survey()->create($data['survey']);
-//        $survey->response()->createMany($data['responses']);
+        foreach ($loop as $value) {
+            $survey = new SurveyResponse();
+            $survey->survey_id = $sur->id;
+            $survey->answer_id = $value;
+            $survey->save();
 
 
-            $user=Auth::user();
-            //dd(request()->get('answer'));
-            $loop= request()->get('answer');
-            
-            foreach ($loop as $value){
-                $survey= new SurveyResponse();
-               $survey->survey_id=$questionaire->id;
-                $survey->answer_id=$value;
-                $survey->save();
-
-
-            }
-
-
-
+        }
+        $id = $sur->id;
+        return redirect()->action(
+            'SurveyResponseController@show', ['id' => $id]
+        );
 
 
     }
